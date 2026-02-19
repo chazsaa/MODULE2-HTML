7a24d67f1ab5ac32648b31ca9aae1825fdbc9b92
chazsaa <delavictoria.chariemae08@gmail.com>
2026-02-19 15:38:00 +0800

    PIT SUBMISSION

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 console.log("js/script.js");
 
-const submitBtn = document.getElementById("submitBtn");
+
 
 const themeToggle = document.getElementById("themeToggle");
 
+
 themeToggle.addEventListener("click",function(){
-    document.body.classList.toggle("dark-mode");
+    const inDark = document.body.classList.toggle("dark-mode") 
+    themeToggle.textContent = inDark ? "Toggle Light Mode" : "Toggle Dark Mode";
 });
 
 const toggleSkills = document.getElementById("toggleSkills");
@@ -15,6 +17,7 @@ toggleSkills.addEventListener("click",function(){
     skillsSection.classList.toggle("hidden");
 });
 
+const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click",function(event){
     event.preventDefault();
     const name = document.getElementById("name").value;
